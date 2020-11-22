a0ba3e9e6d89908cb0e5a14e7e95bc49b9865be1
Miotis2004 <miotis2004@gmail.com>
2020-11-22 14:02:42 -0500

    Added media

---
+++ Africa/Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Field Employee on 11/22/20.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
