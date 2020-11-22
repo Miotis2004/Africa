f02c6725918ca60628afbdd0ed6a3b573fe56f11
Miotis2004 <miotis2004@gmail.com>
2020-11-22 12:56:06 -0500

    First commit

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Field Employee on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
